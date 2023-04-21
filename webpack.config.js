c0f1d922a02294ac19f80efb68e12935e77f6e5e
giry retno indra pangestu <giryindra28@gmail.com>
2023-04-22 01:11:25 +0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
     entry: './src/main.js',
     output: {
-        filename: 'app.js',
+        filename: 'testing.js',
         path: path.resolve(__dirname, '.')
     }
 }
